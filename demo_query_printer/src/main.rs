574e26b27d87b7462708d77a5630ac2cfec548e5
Parthgupta0605 <cs1230507@iitd.ac.in>
2026-04-25 05:28:15 +0530

    updated hash join

--- demo_query_printer/src/main.rs
+++ demo_query_printer/src/main.rs
@@ -1,25 +1,35 @@
 use common::query::{
     ComparisionOperator, ComparisionValue, MultiSortBuilder, MultiProjectBuilder, QueryOp,
 };
-
 fn main() {
-    let query = QueryOp::scan("lineitem")
-        .filter(
-            "l_shipdate",
-            ComparisionOperator::LTE,
-            ComparisionValue::String(String::from("1998-09-02")),
-        )
+    let query = QueryOp::scan("part")
+        .cross(QueryOp::scan("supplier"))
+        .cross(QueryOp::scan("partsupp"))
+        .cross(QueryOp::scan("nation"))
+        .cross(QueryOp::scan("region"))
+        .filter("p_partkey", ComparisionOperator::EQ, ComparisionValue::Column("ps_partkey".into()))
+        .filter("s_suppkey", ComparisionOperator::EQ, ComparisionValue::Column("ps_suppkey".into()))
+        .filter("p_size", ComparisionOperator::EQ, ComparisionValue::I32(15))
+        .filter("s_nationkey", ComparisionOperator::EQ, ComparisionValue::Column("n_nationkey".into()))
+        .filter("n_regionkey", ComparisionOperator::EQ, ComparisionValue::Column("r_regionkey".into()))
+        .filter("r_name", ComparisionOperator::EQ, ComparisionValue::String("EUROPE".into()))
         .sort_multiple(
-            MultiSortBuilder::new("l_returnflag", true)
-                .add("l_linestatus", true)
+            MultiSortBuilder::new("s_acctbal", false)
+                .add("n_name", true)
+                .add("s_name", true)
+                .add("p_partkey", true)
+                .add("ps_suppkey", true)
         )
         .project_multiple(
-            MultiProjectBuilder::new("l_returnflag", "l_returnflag")
-                .add("l_linestatus", "l_linestatus")
-                .add("l_quantity", "l_quantity")
-                .add("l_extendedprice", "l_extendedprice")
-                .add("l_discount", "l_discount")
-                .add("l_tax", "l_tax")
+            MultiProjectBuilder::new("s_acctbal", "s_acctbal")
+                .add("s_name", "s_name")
+                .add("n_name", "n_name")
+                .add("p_partkey", "p_partkey")
+                .add("p_mfgr", "p_mfgr")
+                .add("s_address", "s_address")
+                .add("s_phone", "s_phone")
+                .add("s_comment", "s_comment")
+                .add("ps_supplycost", "ps_supplycost")
         )
         .build();
 
